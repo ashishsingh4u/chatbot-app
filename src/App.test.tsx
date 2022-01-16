3946211a334daf2a63a62765aa900eeba799fa51
Ashish Singh <ashishsingh4u@gmail.com>
2022-01-17 01:43:08 +0800

    Tests added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,20 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders Questions Tab", () => {
   render(<App />);
   const linkElement = screen.getAllByText(/Questions/i);
+  expect(linkElement).toBeTruthy();
+});
+
+test("renders Categories Tab", () => {
+  render(<App />);
+  const linkElement = screen.getAllByText(/Categories/i);
+  expect(linkElement).toBeTruthy();
+});
+
+test("renders Admin Tab", () => {
+  render(<App />);
+  const linkElement = screen.getAllByText(/Admin/i);
   expect(linkElement).toBeTruthy();
 });
